4851f28b67ab6fb3db69fc911a00dc527ec7e25b
Sundara-Rajan <68390761+Sundara-Rajan@users.noreply.github.com>
2022-04-13 19:40:46 +0530

    Update ArrayAverage.java

--- ArrayAverage.java
+++ ArrayAverage.java
@@ -11,3 +11,5 @@ class ArrayAverage
 		System.out.println(sum/arr.length);
 	}
 }
+
+//https://click.pstmrk.it/2sm/www.hackerrank.com%2Ftests%2F4ckgrl6008g%2Flogin%3Fb%3DeyJ1c2VybmFtZSI6InN1bmR1MTA0ZWNlQGdtYWlsLmNvbSIsInBhc3N3b3JkIjoiZDk3MDkxMzQiLCJoaWRlIjp0cnVlLCJhY2NvbW1vZGF0aW9ucyI6eyJhZGRpdGlvbmFsX3RpbWVfcGVyY2VudCI6MH19/YRqCHywN/EDcI/qpnbU-4J4o/aHJ3LXRlc3QtaW52aXRl
