1cf5bb7580e9a517ee4706233c45903cc05707f3
Sundara-Rajan <68390761+Sundara-Rajan@users.noreply.github.com>
2022-04-17 22:18:17 +0530

    Update ArrayAverage.java

--- ArrayAverage.java
+++ ArrayAverage.java
@@ -13,3 +13,7 @@ class ArrayAverage
 }
 
 //https://click.pstmrk.it/2sm/www.hackerrank.com%2Ftests%2F4ckgrl6008g%2Flogin%3Fb%3DeyJ1c2VybmFtZSI6InN1bmR1MTA0ZWNlQGdtYWlsLmNvbSIsInBhc3N3b3JkIjoiZDk3MDkxMzQiLCJoaWRlIjp0cnVlLCJhY2NvbW1vZGF0aW9ucyI6eyJhZGRpdGlvbmFsX3RpbWVfcGVyY2VudCI6MH19/YRqCHywN/EDcI/qpnbU-4J4o/aHJ3LXRlc3QtaW52aXRl
+
+
+
+https://tests.mettl.com/authenticateKey/2p5hlbgidd
